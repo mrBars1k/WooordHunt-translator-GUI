c8ae4764e8bffd343117945c671601f8ef2f96d7
mrBars1k <elxlawliet@gmail.com>
2024-03-08 11:30:47 +0200

    Empty tk window

--- main.py
+++ main.py
@@ -2,6 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 import urllib3
 
+from tkinter import *
+from tkinter import ttk
+import psycopg2
+from psycopg2 import sql
+from tk import *
+
+## ## ## ## ## ## ## ##
+
+adb = psycopg2.connect(
+    dbname=dbname,
+    user=user,
+    password=password,
+    host=host,
+    port=port)
+
+cur = adb.cursor()
+
+## ## ## ## ## ## ## ##
+
 def take_word(word):
     url = requests.get(f"https://wooordhunt.ru/word/{word}", verify=False)
     resp_check = url.text
@@ -16,3 +35,20 @@ def take_word(word):
         return f"{word}{transcript} {translate}"
     except:
         return word
+
+## ## ## ## ## ## ## ##
+
+mainw = Tk() ## main window;
+mainw.title("WOORDHUNT") ## headline;
+mainw.geometry("1920x1080") ## resolution;
+mainw.wm_minsize(1280, 720)  ## min width and hight;
+mainw.wm_maxsize(1920, 1080)  ## max width and hight;
+
+## ## ## ## ## ## ## ##
+
+
+
+## ## ## ## ## ## ## ##
+mainw.mainloop()
+cur.close()
+adb.close()
