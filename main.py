e07c1df5afb928d8644b8421851b6fd153b42113
mrBars1k <elxlawliet@gmail.com>
2024-03-08 11:25:25 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib3
+
+def take_word(word):
+    url = requests.get(f"https://wooordhunt.ru/word/{word}", verify=False)
+    resp_check = url.text
+    soup = BeautifulSoup(resp_check, "html.parser")
+    try:
+        translate = soup.find("div", class_="t_inline_en").text
+        transcript = soup.find(
+            "span",
+            class_="transcription",
+            title=f"британская транскрипция слова {word}",
+        ).text
+        return f"{word}{transcript} {translate}"
+    except:
+        return word
